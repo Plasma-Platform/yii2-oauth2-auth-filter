cb9d2858c6c26b97cc6a8d2b97d53d572660bc6f
utrain <pavelterletskiy@gmail.com>
2021-04-15 17:37:39 +0300

    fix rawResponse on test run

--- src/authfilter/components/TestHelper.php
+++ src/authfilter/components/TestHelper.php
@@ -10,16 +10,16 @@ class TestHelper
     public static function getTokenInfo($rawResponse = false)
     {
         $tokenInfo = [
-            'owner_id'     => '1',
-            'owner_type'   => 'user',
+            'owner_id' => '1',
+            'owner_type' => 'user',
             'access_token' => 'token',
-            'client_id'    => '1',
-            'scopes'       => ''
+            'client_id' => '1',
+            'scopes' => ''
         ];
         if (Yii::$app->getRequest()->getHeaders()->get('X-Token-info') !== null) {
             $tokenInfo = array_merge($tokenInfo, json_decode(Yii::$app->getRequest()->getHeaders()->get('X-Token-info'), true));
         }
-        if($rawResponse==false){
+        if ($rawResponse == false) {
             return $tokenInfo;
         }
         $response = new Response();
